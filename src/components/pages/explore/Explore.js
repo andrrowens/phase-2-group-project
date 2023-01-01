2877ddbc641061115a56d11f43ecefb4c5891bfa
Genevieve Suder <genevievesuder@gmail.com>
2023-01-01 15:49:56 -0800

    new files

---
+++ src/components/pages/explore/Explore.js
@@ -0,0 +1,14 @@
+import Park from "./Park"
+import ExploreHeader from "./ExploreHeader";
+
+const Explore = ({parks, searchPark, setSearchPark}) => {
+    const mappedParks = parks.map(park => <Park {...park} key={park.id} searchPark={searchPark} setSearchPark={setSearchPark}/>);
+    return(
+       <div className="parks">
+        <ExploreHeader searchPark={searchPark} setSearchPark={setSearchPark}/>
+        {mappedParks}
+       </div>
+    )
+}
+
+export default Explore;
---
+++ src/components/pages/homepage/Home.js
@@ -0,0 +1,11 @@
+
+const Home = () => {
+    return(
+        <div className="home">
+        <h1>HomePage</h1>
+        <h2>Blank canvas! What should we add here?</h2>
+        </div>
+    )
+}
+
+export default Home;
