d2ed58942a2233c89d8807113f68348406e2480d
Sarah Nicole <110745384+srodarte1@users.noreply.github.com>
2023-01-03 17:03:14 -0700

    Revert "Andrew branch"

--- src/components/pages/reviews/Reviews.js
+++ src/components/pages/reviews/Reviews.js
@@ -1,52 +1,7 @@
-<<<<<<< .merge_file_af6HVE
-import React from "react";
-import { useState, useEffect } from "react"
-import ReviewsPage from "./ReviewsPage"
-import NewReview from "./NewReview"
+import React from 'react'
 
-const Reviews = ({parks}) => {
-
-   console.log(parks.name)
-
-
-    const [reviews, setReviews] = useState([])
-
-      useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const resp = await fetch("http://localhost:3000/reviews")
-            const data = await resp.json()
-            setReviews(data)
-            console.log(data)
-          } catch (error) {
-            alert(error)
-          }
-      }
-      fetchData()
-      }, []);
-
+export default function Reviews() {
   return (
-    <div className="reviews-home"> 
-      <h2 className="review-header"> Recent Visitor Reviews</h2>
-      <div className="review-container">
-        <NewReview setReviews={setReviews} parks={parks} />
-      </div>
-      <div className="reviews-container">
-        <ReviewsPage reviews={reviews} setReviews={setReviews} />
-      </div>
-    </div>
-  );
+    <div>Reviews</div>
+  )
 }
-
-export default Reviews;
-
-=======
-
-const Reviews = () => {
-    return(
-        <h1>Reviews</h1>
-    )
-}
-
-export default Reviews;
->>>>>>> .merge_file_eG2MQM
