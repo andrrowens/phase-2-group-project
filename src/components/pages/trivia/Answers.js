42137e0fcce35b2a174e3cb48c8c4cdb5779f343
+ <rodarte_sarah@yahoo.com>
2023-01-04 20:39:35 -0700

    added gameover

--- src/components/pages/trivia/Answers.js
+++ src/components/pages/trivia/Answers.js
@@ -1,20 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const Answers = ({answerOptions : {answerOptions}}) => {
-  console.log(answerOptions)
-  // const [currentQuestion, setCurrentQuestion] = useState(0);
+const GameOver = ({points})=>{
+  const navigateHome = useNavigate()
+  const navigateTrivia = useNavigate()
+  
+
+  const handleGoHome = ()=> {
+  return navigateHome('/') 
+  } 
+  const handleGoTrivia = ()=> {
+  return navigateTrivia('/trivia')
+  }
  
-  // const handleClick = (e) => {
-  //   setCurrentQuestion(item.isCorrect)}
 
   
   return (
-   <div>
-    <button>Answers </button>
-    <button>Answers </button>
-    <button>Answers </button>
-    <button>Answers </button>
-   </div>
-  )
-            }
-export default Answers
+    <div className="game-over">
+    <h1>Game Over</h1>
+    <h2>Total Points: {points}</h2>
+    <button onClick={handleGoTrivia} className="play-again">Play Again</button>
+    <button onClick={handleGoHome} className="back-home">Back Home</button>
+    </div>)
+}
+export default GameOver
